143075ae316c9e3d688c3826fe1428ebfbd73593
Marat Yamborisov <yamborisov.ma@phystech.edu>
2021-10-07 21:37:30 +0300

    add template

---
+++ YandexPractikum/10th_sprint/binary_tree_with_unique_ptr/cmake-build-debug/CMakeFiles/binary_tree_with_unique_ptr.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binary_tree_with_unique_ptr.dir/main.cpp.obj"
+  "binary_tree_with_unique_ptr.exe"
+  "binary_tree_with_unique_ptr.exe.manifest"
+  "binary_tree_with_unique_ptr.lib"
+  "binary_tree_with_unique_ptr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binary_tree_with_unique_ptr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
