70af3ef728150ec2da92ae5ea017c9abbcea4b88
Marat Yamborisov <yamborisov.ma@phystech.edu>
2021-10-31 14:38:45 +0300

    add compute statistics

--- YandexPractikum/12th_sprint/compute_statistics/main.cpp
+++ YandexPractikum/12th_sprint/compute_statistics/main.cpp
@@ -7,8 +7,47 @@
 
 using namespace std;
 
-// напишите функцию ComputeStatistics, принимающую 5 параметров:
-// два итератора, выходное значение для суммы, суммы квадратов и максимального элемента
+template <typename InputIt, typename OutSum, typename OutSqSum, typename OutMax>
+void ComputeStatistics(InputIt first, InputIt last, OutSum& out_sum, OutSqSum& out_sq_sum, OutMax& out_max) {
+    if (first == last) {
+        return;
+    }
+    using Element = std::decay_t<decltype(*first)>;
+    constexpr bool need_sum = !is_same_v<OutSum, const nullopt_t>;
+    Element temp_sum;
+    if constexpr (need_sum) {
+        temp_sum = *first;
+    }
+    constexpr bool need_sq_sum = !is_same_v<OutSqSum, const nullopt_t>;
+    Element temp_sq_sum;
+    if constexpr (need_sq_sum)
+        temp_sq_sum = (*first) * (*first);
+    constexpr bool need_max = !is_same_v<OutMax, const nullopt_t>;
+    Element temp_max;
+    if constexpr (need_max)
+        temp_max = *first;
+    for (auto it = next(first); it != last; it = next(it)) {
+        if constexpr (need_sum) {
+            temp_sum += *it;
+        }
+        if constexpr (need_sq_sum) {
+            temp_sq_sum += *it * *it;
+        }
+        if constexpr (need_max) {
+            if (*it > temp_max)
+                temp_max = *it;
+        }
+    }
+    if constexpr (need_sum) {
+        out_sum = temp_sum;
+    }
+    if constexpr (need_sq_sum) {
+        out_sq_sum = temp_sq_sum;
+    }
+    if constexpr (need_max) {
+        out_max = temp_max;
+    }
+}
 
 struct OnlySum {
     int value;
