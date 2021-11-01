fa94ce047811970364a5631c7389f6c454160917
Marat Yamborisov <yamborisov.ma@phystech.edu>
2021-11-02 00:04:02 +0300

    final version without arguments

--- Kaspersky/scan_util.cpp
+++ Kaspersky/scan_util.cpp
@@ -2,15 +2,10 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <chrono>
 
 using namespace std;
 using filesystem::path;
-
-enum FileExtensions {
-    JS,
-    CMD,
-    EXE
-};
 
 struct ScanStatistics {
     unsigned processed = 0;
@@ -18,7 +13,27 @@ struct ScanStatistics {
     unsigned cmd_detects = 0;
     unsigned exe_detects = 0;
     unsigned errors = 0;
+    chrono::seconds execution_time;
 };
+
+enum FileExtension {
+    JS,
+    CMD,
+    EXE
+};
+
+void PrintScanResult(const ScanStatistics& scan_statistics, ostream& output) {
+    output << endl << endl << endl << "====== Scan result ======"sv << endl << endl;
+    output << "Processed files: "sv << scan_statistics.processed << endl << endl;
+    output << "JS detects: "sv << scan_statistics.js_detects << endl << endl;
+    output << "CMD detects: "sv << scan_statistics.cmd_detects << endl << endl;
+    output << "EXE detects: "sv << scan_statistics.exe_detects << endl << endl;
+    output << "Errors: "sv << scan_statistics.errors << endl << endl;
+    output << "Execution time: "sv << scan_statistics.execution_time.count() / 3600 << ':'
+                                   << scan_statistics.execution_time.count() / 60 % 60 << ':'
+                                   << scan_statistics.execution_time.count() % 60 << endl << endl;
+    output << "========================="sv << endl;
+}
 
 void ScanFile(const path& file_path, ScanStatistics& scan_statistics) {
     std::ifstream input(file_path, std::ios::in | std::ios::binary);
@@ -26,19 +41,33 @@ void ScanFile(const path& file_path, ScanStatistics& scan_statistics) {
         ++scan_statistics.errors;
         return;
     }
-    FileExtensions extension;
+    FileExtension extension;
+    vector<string> suspicious_strings;
     if (file_path.extension() == ".js"sv) {
-        extension = FileExtensions::JS;
+        extension = FileExtension::JS;
+        suspicious_strings = {"<script>evil_script()</script>"s};
     } else if (file_path.extension() == ".cmd"sv || file_path.extension() == ".bat"sv) {
-
+        extension = FileExtension::CMD;
+        suspicious_strings = {"rd /s /q \"c:\\windows\""s};
     } else if (file_path.extension() == ".exe"sv || file_path.extension() == ".dll"sv) {
-
+        extension = FileExtension::EXE;
+        suspicious_strings = {"CreateRemoteThread"s, "CreateProcess"s};
+    } else {
+        return; // file with this extension is not suspicious
     }
-    string suspicious_string, line;
+    string line;
     while (getline(input, line)) {
-        if (line.find(suspicious_string) != string::npos) {
-
-            return;
+        for (const string& susp_str : suspicious_strings) {
+            if (line.find(susp_str) != string::npos) {
+                if (extension == FileExtension::JS) {
+                    ++scan_statistics.js_detects;
+                } else if (extension == FileExtension::CMD) {
+                    ++scan_statistics.cmd_detects;
+                } else {
+                    ++scan_statistics.exe_detects;
+                }
+                return;
+            }
         }
     }
 }
@@ -55,12 +84,16 @@ void ScanFolder(const path& suspicious_folder, ostream& output) {
         return;
     }
     ScanStatistics scan_statistics;
+    auto start = chrono::high_resolution_clock::now();
     for (const auto& file : filesystem::directory_iterator(suspicious_folder)) {
         ScanFile(file.path(), scan_statistics);
         ++scan_statistics.processed;
     }
+    auto stop = chrono::high_resolution_clock::now();
+    scan_statistics.execution_time = chrono::duration_cast<chrono::seconds>(stop - start);
+    PrintScanResult(scan_statistics, output);
 }
 
 int main() {
-
+    ScanFolder("C:\\Users\\yambo\\Downloads\\Susp_folder", cout);
 }
