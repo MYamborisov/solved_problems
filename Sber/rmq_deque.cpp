34a62a62c3415436ce36a93dc6e62a43ed41c635
Marat Yamborisov <yamborisov.ma@phystech.edu>
2021-11-11 19:47:31 +0300

    improved asymptotic, removed index storage

--- Sber/rmq_deque.cpp
+++ Sber/rmq_deque.cpp
@@ -7,32 +7,25 @@
 
 using namespace std;
 
-struct Num {
-    size_t pos;
-    int value;
-};
-
 void Solution(const vector<int>& numbers, istream& in, ostream& out) {
     string cmd;
     int L = 0, R = 0;
-    deque<Num> possible_max = {{0, numbers[0]}};
+    deque<int> possible_max = {numbers[0]};
     while (in >> cmd) {
         if (cmd == "L") {
-            if (possible_max.front().pos < ++L) {
+            if (possible_max.front() == numbers[L++]) {
                 possible_max.pop_front();
             }
         } else if (cmd == "R") {
-            Num new_val;
-            new_val.value = numbers[++R];
-            new_val.pos = R;
-            auto it = (upper_bound(possible_max.rbegin(), possible_max.rend(), new_val, [](const Num& lhs, const Num& rhs){
-                return lhs.value < rhs.value;
-            }));
+            int new_val = numbers[++R];
+            auto it = find_if(possible_max.rbegin(), possible_max.rend(), [new_val](int value){
+                return value > new_val;
+            });
             int dist =  distance(possible_max.rbegin(), it);
             possible_max.erase(possible_max.end() - dist, possible_max.end());
             possible_max.push_back(new_val);
         }
-        out << possible_max.front().value << endl;
+        out << possible_max.front() << endl;
     }
 }
 
