22555968dac7dea72f117f7a7d780ad6e9377bbe
Marat Yamborisov <yamborisov.ma@phystech.edu>
2021-11-27 23:01:01 +0300

    add NegateInplace

--- YandexPractikum/14th_sprint/images/main.cpp
+++ YandexPractikum/14th_sprint/images/main.cpp
@@ -5,17 +5,29 @@
 
 using namespace std;
 
+void NegateInplace(img_lib::Image& image) {
+    for (int y = 0; y < image.GetHeight(); ++y) {
+        for (int x = 0; x < image.GetWidth(); ++x) {
+            image.GetPixel(x, y).r = byte(255 - to_integer<int>(image.GetPixel(x, y).r));
+            image.GetPixel(x, y).g = byte(255 - to_integer<int>(image.GetPixel(x, y).g));
+            image.GetPixel(x, y).b = byte(255 - to_integer<int>(image.GetPixel(x, y).b));
+        };
+    }
+}
+
 int main(int argc, const char** argv) {
 //    if (argc != 3) {
 //        cerr << "Usage: "sv << argv[0] << " <input image> <output image>"sv << endl;
 //        return 1;
 //    }
 
-    const auto image = img_lib::LoadPPM("1.kissy++.ppm"s);
+    auto image = img_lib::LoadPPM("1.kissy++.ppm"s);
     if (!image) {
         cerr << "Error loading image"sv << endl;
         return 2;
     }
+
+    NegateInplace(image);
 
     if (!img_lib::SavePPM("2.kissy++.ppm"s, image)) {
         cerr << "Error saving image"sv << endl;
